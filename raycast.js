f4a06a7fcb1c9680d30847dda75c68ac30e10246
Fr3ud <oo.pavelko@gmail.com>
2020-02-19 15:19:54 +0200

    add Player class

--- raycast.js
+++ raycast.js
@@ -36,7 +36,28 @@ class Map {
     }
 }
 
+class Player {
+    constructor() {
+        this.x = WINDOW_WIDTH / 2;
+        this.y = WINDOW_HEIGHT / 2;
+        this.radius = 3;
+        this.turnDirection = 0; // -1 if left, +1 if right
+        this.walkDirection = 0; // -1 if back, +1 if front
+        this.rotationAngle = Math.PI / 2;
+        this.moveSpeed = 2.0;
+        this.rotationSpeed = 2 * (Math.PI / 180);
+    }
+    update() {
+        // TODO: update player position based on turnDirection and walkDirection
+    }
+    render() {
+        fill("red");
+        circle(this.x, this.y, this.radius);
+    }
+}
+
 var grid = new Map();
+var player = new Player();
 
 function setup() {
     // TODO: initialize all objects
@@ -46,10 +67,12 @@ function setup() {
 
 function update() {
     // TODO: upadate all game objects before we render the next frame
+    player.update();
 }
 
 function draw() {
     update();
     // TODO: render all objects frame by frame
     grid.render();
+    player.render();
 }
